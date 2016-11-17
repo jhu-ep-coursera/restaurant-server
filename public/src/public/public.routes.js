6e3d3e15ee6da3c860001cf388fa47b9d2e4a8a9
Yaakov Chaikin <yaakov.chaikin@gmail.com>
2016-11-17 18:26:24 -0500

    Porting all the front-end to the ror app.

---
+++ public/src/public/public.routes.js
@@ -0,0 +1,53 @@
+(function() {
+'use strict';
+
+angular.module('public')
+.config(routeConfig);
+
+/**
+ * Configures the routes and views
+ */
+routeConfig.$inject = ['$stateProvider'];
+function routeConfig ($stateProvider) {
+  // Routes
+  $stateProvider
+    .state('public', {
+      absract: true,
+      templateUrl: 'src/public/public.html'
+    })
+    .state('public.home', {
+      url: '/',
+      templateUrl: 'src/public/home/home.html'
+    })
+    .state('public.menu', {
+      url: '/menu',
+      templateUrl: 'src/public/menu/menu.html',
+      controller: 'MenuController',
+      controllerAs: 'menuCtrl',
+      resolve: {
+        menuCategories: ['MenuService', function (MenuService) {
+          return MenuService.getCategories();
+        }]
+      }
+    })
+    .state('public.menuitems', {
+      url: '/menu/{category}',
+      templateUrl: 'src/public/menu-items/menu-items.html',
+      controller: 'MenuItemsController',
+      controllerAs: 'menuItemsCtrl',
+      resolve: {
+        menuItems: ['$stateParams','MenuService', function ($stateParams, MenuService) {
+          return MenuService.getMenuItems($stateParams.category);
+        }]
+      }
+    })
+    .state('public.about', {
+      url: '/about',
+      templateUrl: 'src/public/about.html'
+    })
+    .state('public.awards', {
+      url: '/awards',
+      templateUrl: 'src/public/awards.html'
+    });
+}
+})();
