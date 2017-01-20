a7799ff52f63323b1e4605d1ac3a3d97d1f6eef0
Yaakov Chaikin <yaakov.chaikin@gmail.com>
2017-01-19 19:44:55 -0500

    Changed base URL to be without SSL for now.

--- public/src/common/common.module.js
+++ public/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://www.davidchuschinabistro.com')
+.constant('ApiPath', 'http://www.davidchuschinabistro.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
