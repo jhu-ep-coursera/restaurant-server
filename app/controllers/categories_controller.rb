a4a53bb8766a8d6c60ea12d2ef78093191479b1a
Kalman Hazins <kalmanh@gmail.com>
2019-08-21 13:09:58 -0400

    Sort categories by id

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,7 +7,7 @@ class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :update, :destroy]
 
   def index
-    @categories = Category.all
+    @categories = Category.all.order(:id)
   end
 
   def show
